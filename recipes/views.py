19cfcb9b37d39d70dcc7a4abb9e7b4342e612e6e
Heitor Teno Müller <heitortmuller@gmail.com>
2022-05-23 20:51:19 -0300

    Desenvolvimento do render

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Heitor Müller',
+    })
 
 
 def contato(request):
