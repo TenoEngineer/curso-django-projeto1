1b32c21f46d0e48b2fb20f8af16574013de84da8
Heitor Teno Müller <heitortmuller@gmail.com>
2022-10-24 05:42:09 -0300

    admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
 # Register your models here.
