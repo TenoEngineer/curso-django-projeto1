0598e5b359114318ba939362d7b1e37ed18cd1a0
Heitor Teno Müller <heitortmuller@gmail.com>
2022-10-24 06:16:43 -0300

    adding recipes on admin

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     name = models.CharField(max_length=65)
 
     def __str__(self) -> str:
-        return self.name()
+        return self.name
 
 
 class Recipe(models.Model):
@@ -27,3 +27,6 @@ class Recipe(models.Model):
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.title
