f02fd8dcf280aa492ff954accbabc422b0a1f5c2
Heitor Teno Müller <heitortmuller@gmail.com>
2022-11-25 09:28:02 -0300

    ajustes finais do site

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 from ultis.recipes.factory import make_recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -25,7 +29,7 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
+    recipe = get_object_or_404(Recipe, id=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True
