2b84d6eb5891716b09c19bef95532d684120f86a
TenoEngineer <heitortmuller@gmail.com>
2022-12-10 20:22:58 -0300

    continuando a aprender testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
 from recipes.models import Recipe
-from ultis.recipes.factory import make_recipe
 
 
 def home(request):
@@ -33,9 +33,9 @@ def recipe(request, id):
         'is_detail_page': True
     })
 
+
 def search(request):
     search_term = request.GET.get('q')
-
     if not search_term:
         raise Http404()
     return render(request, 'recipes/pages/search.html')
