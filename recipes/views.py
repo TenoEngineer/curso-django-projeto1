638043410e0fe17a04e58c9c8a45b8a432816f8a
TenoEngineer <heitortmuller@gmail.com>
2022-12-11 08:51:47 -0300

    Finalizando testes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
@@ -35,7 +36,18 @@ def recipe(request, id):
 
 
 def search(request):
-    search_term = request.GET.get('q')
+    search_term = request.GET.get('q', '').strip()
+
     if not search_term:
         raise Http404()
-    return render(request, 'recipes/pages/search.html')
+
+    recipes = Recipe.objects.filter(
+        Q(Q(title__contains=search_term) |
+          Q(description__contains=search_term)), is_published=True)
+    recipes = recipes.order_by('-id')
+
+    return render(request, 'recipes/pages/search.html',
+                  {'page_title': f'Search for "{search_term}" | ',
+                   'search_term': search_term,
+                   'recipes': recipes
+                   })
